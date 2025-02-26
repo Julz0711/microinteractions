aee8369cc93dc066194a5103eae9920840e6398c
Julz0711 <julian.gkt@gmail.com>
2025-02-26 12:26:29 +0100

    uggo on off button

--- src/components/DeviceSettings/OnOffButton.tsx
+++ src/components/DeviceSettings/OnOffButton.tsx
@@ -1,9 +1,10 @@
-import { twMerge } from "tailwind-merge";
-import DynamicIcon from "../DynamicIcon";
-import { getColor } from "../../helpers/helpers";
-import { Category } from "../../types/dashboard.types";
-import { setIsOn } from "../../store/reducer";
-import { useDispatch } from "react-redux";
+import { twMerge } from 'tailwind-merge';
+import DynamicIcon from '../DynamicIcon';
+import { getColor } from '../../helpers/helpers';
+import { Category } from '../../types/dashboard.types';
+import { setIsOn } from '../../store/reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState } from '../../store/store';
 
 export interface IButtonProps {
   category: Category;
@@ -11,6 +12,10 @@ export interface IButtonProps {
 }
 
 const OnOffButton = (props: IButtonProps) => {
+  const hasMicrointeractions = useSelector(
+    (state: AppState) => state.app.hasMicrointeractions
+  );
+
   const dispatch = useDispatch();
   const buttonColor = getColor(props.category);
 
@@ -21,14 +26,15 @@ const OnOffButton = (props: IButtonProps) => {
   return (
     <button
       className={twMerge(
-        "mb-8 p-5 duration-150 shadow-2xl rounded-full cursor-pointer",
+        hasMicrointeractions ? 'duration-150' : '',
+        'mb-8 p-5 shadow-2xl rounded-full cursor-pointer',
         props.isOn
           ? `${buttonColor} hover:bg-dark text-light`
-          : "bg-light/50 hover:bg-light text-dark"
+          : 'bg-light/50 hover:bg-light text-dark'
       )}
       onClick={handleClick}
     >
-      <DynamicIcon iconName="OnOff" size={"40"} />
+      <DynamicIcon iconName="OnOff" size={'40'} />
     </button>
   );
 };
