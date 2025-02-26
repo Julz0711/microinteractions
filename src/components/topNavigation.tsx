b75de2c2db1d1bbffa1db8bdf188a2401b9eb1ef
Julz0711 <julian.gkt@gmail.com>
2025-02-26 13:13:30 +0100

    profile page auf süß, neue icons

--- src/components/topNavigation.tsx
+++ src/components/topNavigation.tsx
@@ -1,38 +1,38 @@
-import { Link, useLocation } from "react-router-dom";
-import { twMerge } from "tailwind-merge";
+import { Link, useLocation } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 export const TopNavigation = ({ isScrolled }: { isScrolled: boolean }) => {
   const location = useLocation();
 
   const linkClasses = (path: string) =>
     `transition-colors text-nav font-bold ${
-      location.pathname === path ? "text-dark" : "text-uwu"
+      location.pathname === path ? 'text-dark' : 'text-uwu'
     } `;
 
   const profileButtonClasses = `w-12 h-12 font-bold text-light text-[.75rem] rounded-full flex items-center justify-center transition-colors ${
-    location.pathname === "/profile" ? "bg-dark" : "bg-uwu"
+    location.pathname === '/profil' ? 'bg-dark' : 'bg-uwu'
   }`;
 
   return (
     <nav
       className={twMerge(
-        "fixed top-0 left-0 w-full pt-8 pb-4 z-50 backdrop-blur-sm bg-light duration-150",
-        isScrolled ? "shadow-lg" : "shadow-none"
+        'fixed top-0 left-0 w-full pt-8 pb-4 z-50 backdrop-blur-sm bg-light duration-150',
+        isScrolled ? 'shadow-lg' : 'shadow-none'
       )}
     >
       <div className="px-800 flex justify-between items-center">
         <div className="flex gap-900">
-          <Link to="/dashboard" className={linkClasses("/dashboard")}>
+          <Link to="/dashboard" className={linkClasses('/dashboard')}>
             Dashboard
           </Link>
-          <Link to="/geraete" className={linkClasses("/geraete")}>
+          <Link to="/geraete" className={linkClasses('/geraete')}>
             Geräte
           </Link>
         </div>
 
         <div className="flex items-center">
-          <Link to="/profile" className={profileButtonClasses}>
-            JG
+          <Link to="/profil" className={profileButtonClasses}>
+            AS
           </Link>
         </div>
       </div>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,63 @@
-import React from 'react';
+import DynamicIcon from '../components/DynamicIcon';
 import { AppState } from '../store/store';
 import { useSelector } from 'react-redux';
+
+const settings = [
+  {
+    icon: 'User',
+    title: 'Account'
+  },
+  {
+    icon: 'Lock',
+    title: 'Privatsphäre'
+  },
+  {
+    icon: 'Home',
+    title: 'Homes'
+  },
+  {
+    icon: 'Connect',
+    title: 'Hubs & Bridges'
+  },
+  {
+    icon: 'Settings',
+    title: 'Erweiterte Einstellungen'
+  }
+];
 
 const Profile = () => {
   const hasMicrointeractions = useSelector(
     (state: AppState) => state.app.hasMicrointeractions
   );
   return (
-    <div>
-      <div className="avatar">
-        <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
-          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    <div className="pt-32 flex flex-col gap-12 items-center w-full justify-center">
+      <div className="flex flex-col gap-6 items-center w-full justify-center">
+        <div className="avatar">
+          <div className="ring-red ring-4 ring-offset-base-800 w-40 rounded-full ring ring-offset-4">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
         </div>
+        <div className="font-bold text-xl text-dark">Alex Smart</div>
+      </div>
+      <div className="bg-inactive p-5 w-full rounded-md flex flex-col gap-6">
+        {settings.map((setting, index) => (
+          <div key={index} className="">
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex flex-row gap-2">
+                <div className="w-6 flex items-center justify-center">
+                  <DynamicIcon iconName={setting.icon} size="20" />
+                </div>
+
+                <div className="text-dark font-normal">{setting.title}</div>
+              </div>
+              <DynamicIcon
+                iconName={'ChevronRight'}
+                size="18"
+                color="text-uwu"
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
