a2868e559f6e3906dc89445de913a9f6fd2547f6
Julz <julian.gkt@gmail.com>
2025-03-02 20:00:24 +0100

    auto scroll hotspot ezpz

--- src/components/Hotspot/Hotspot.tsx
+++ src/components/Hotspot/Hotspot.tsx
@@ -1,8 +1,7 @@
-import { useState, useLayoutEffect, useRef } from "react";
+import { useState, useLayoutEffect, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import gsap from "gsap";
 import Button from "../Button";
-import DynamicIcon from "../DynamicIcon";
 
 interface HotspotProps {
   targetId: string;
@@ -27,6 +26,7 @@ const Hotspot: React.FC<HotspotProps> = ({
   const [position, setPosition] = useState({ top: 0, left: 0 });
   const holdTimer = useRef<NodeJS.Timeout | null>(null);
   const isHolding = useRef(false);
+  const messageRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
     const targetElement = document.getElementById(targetId);
@@ -48,6 +48,15 @@ const Hotspot: React.FC<HotspotProps> = ({
       ease: "power1.inOut",
     });
   }, [targetId]);
+
+  useEffect(() => {
+    if (isOpen && messageRef.current) {
+      messageRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  }, [isOpen]);
 
   const handleClick = () => {
     if (!isHolding.current) {
@@ -126,6 +135,7 @@ const Hotspot: React.FC<HotspotProps> = ({
           ></div>
 
           <motion.div
+            ref={messageRef}
             className="absolute z-[999] p-3 bg-light shadow-xl rounded-md w-2/3 flex flex-col gap-2"
             style={{
               top: `${position.top + 30}px`,
