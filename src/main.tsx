d0c6eadd58bcb3423f7db2f4dab524ce6666d809
Julz <julian.gkt@gmail.com>
2025-03-03 22:22:38 +0100

    posthog update

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import App from "./App.tsx";
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY || "", {
-  api_host:
-    import.meta.env.VITE_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
+const posthogKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY || "";
+const posthogHost =
+  import.meta.env.VITE_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com";
+
+posthog.init(posthogKey, {
+  api_host: posthogHost,
   autocapture: true,
+  debug: true,
 });
+
+useEffect(() => {
+  console.log(import.meta.env.VITE_PUBLIC_POSTHOG_HOST);
+  console.log(import.meta.env.VITE_PUBLIC_POSTHOG_KEY);
+}, []);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PostHogProvider client={posthog}>
