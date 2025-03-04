ffeda577d99d70318cb79c9dc50543aa0ad345e0
Julz <julian.gkt@gmail.com>
2025-03-04 16:49:13 +0100

    instructions local storage

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DynamicIcon from "./DynamicIcon";
 import { useSelector } from "react-redux";
 import { AppState } from "../store/store";
@@ -9,10 +9,37 @@ export function Instructions() {
     (state: AppState) => state.app.hasMicrointeractions
   );
 
+  const initializeCheckboxes = () => {
+    const savedCheckboxes = localStorage.getItem("checkboxes");
+    return savedCheckboxes
+      ? JSON.parse(savedCheckboxes)
+      : {
+          quest1: false,
+          quest2: false,
+          quest3: false,
+          quest4: false,
+          quest5: false,
+          quest6: false,
+        };
+  };
+
   const [isOpen, setIsOpen] = useState(false);
+  const [checkboxes, setCheckboxes] = useState(initializeCheckboxes);
+
+  useEffect(() => {
+    localStorage.setItem("checkboxes", JSON.stringify(checkboxes));
+  }, [checkboxes]);
 
   const handleClickMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    setCheckboxes((prevCheckboxes: any) => ({
+      ...prevCheckboxes,
+      [name]: checked,
+    }));
   };
 
   const labelClass = "ml-1";
@@ -28,32 +55,68 @@ export function Instructions() {
         <div className="bg-[#ffffff] p-4 flex flex-col gap-4 max-w-92">
           <h1>Aufgaben</h1>
           <form>
-            <input type="checkbox" id="quest1" name="quest1" />
+            <input
+              type="checkbox"
+              id="quest1"
+              name="quest1"
+              checked={checkboxes.quest1}
+              onChange={handleCheckboxChange}
+            />
             <label className={labelClass} htmlFor="quest1">
               Registriere Dich in der App.
             </label>
             <br />
-            <input type="checkbox" id="quest2" name="quest2" />
+            <input
+              type="checkbox"
+              id="quest2"
+              name="quest2"
+              checked={checkboxes.quest2}
+              onChange={handleCheckboxChange}
+            />
             <label className={labelClass} htmlFor="quest2">
               Füge ein neues Gerät hinzu.
             </label>
             <br />
-            <input type="checkbox" id="quest3" name="quest3" />
+            <input
+              type="checkbox"
+              id="quest3"
+              name="quest3"
+              checked={checkboxes.quest3}
+              onChange={handleCheckboxChange}
+            />
             <label className={labelClass} htmlFor="quest3">
               Führe das Tutorial durch.
             </label>
             <br />
-            <input type="checkbox" id="quest4" name="quest4" />
+            <input
+              type="checkbox"
+              id="quest4"
+              name="quest4"
+              checked={checkboxes.quest4}
+              onChange={handleCheckboxChange}
+            />
             <label className={labelClass} htmlFor="quest4">
               Schalte eine Lampe im Wohnzimmer aus.
             </label>
             <br />
-            <input type="checkbox" id="quest5" name="quest5" />
+            <input
+              type="checkbox"
+              id="quest5"
+              name="quest5"
+              checked={checkboxes.quest5}
+              onChange={handleCheckboxChange}
+            />
             <label className={labelClass} htmlFor="quest5">
               Stelle die Heizung in der Küche auf 20 Grad.
             </label>
             <br />
-            <input type="checkbox" id="quest6" name="quest6" />
+            <input
+              type="checkbox"
+              id="quest6"
+              name="quest6"
+              checked={checkboxes.quest6}
+              onChange={handleCheckboxChange}
+            />
             <label className={labelClass} htmlFor="quest6">
               Stelle den Luftbefeuchter im Bad auf einen Timer für 4 Stunden.
             </label>
