137c4e30aa74b0a900d9a75a89b2c003147fa3cf
Julz0711 <julian.gkt@gmail.com>
2025-03-08 15:50:16 +0100

    show/hide pw

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import DynamicIcon from "./DynamicIcon";
-import { twMerge } from "tailwind-merge";
-import { AppState } from "../store/store";
-import { useSelector } from "react-redux";
+import { useState } from 'react';
+import DynamicIcon from './DynamicIcon';
+import { twMerge } from 'tailwind-merge';
+import { AppState } from '../store/store';
+import { useSelector } from 'react-redux';
 
 interface InputFieldProps {
   type: string;
@@ -15,6 +15,7 @@ interface InputFieldProps {
   error?: string;
   hasIcon?: boolean;
   isSearch?: boolean;
+  isValid?: boolean;
 }
 
 const InputField: React.FC<InputFieldProps> = ({
@@ -28,12 +29,14 @@ const InputField: React.FC<InputFieldProps> = ({
   error,
   hasIcon = true,
   isSearch = false,
+  isValid
 }) => {
   const hasMicrointeractions = useSelector(
     (state: AppState) => state.app.hasMicrointeractions
   );
 
   const [isFocused, setIsFocused] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleFocus = () => {
     setIsFocused(true);
@@ -44,6 +47,10 @@ const InputField: React.FC<InputFieldProps> = ({
     blur(e);
   };
 
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="w-full">
       <div className="relative w-full">
@@ -51,10 +58,10 @@ const InputField: React.FC<InputFieldProps> = ({
           <div
             className={`absolute transform duration-150 pointer-events-none ${
               value || isFocused
-                ? "top-2 text-xs font-bold opacity-100"
-                : "top-1/2 font-normal opacity-50 -translate-y-1/2"
-            } ${isFocused ? "text-purple" : "text-uwu"} ${
-              hasIcon ? "pl-10" : "pl-4"
+                ? 'top-2 text-xs font-bold opacity-100'
+                : 'top-1/2 font-normal opacity-50 -translate-y-1/2'
+            } ${isFocused ? 'text-purple' : 'text-uwu'} ${
+              hasIcon ? 'pl-10' : 'pl-4'
             }`}
           >
             {placeholder}
@@ -62,16 +69,16 @@ const InputField: React.FC<InputFieldProps> = ({
         ) : (
           <div
             className={twMerge(
-              "absolute top-1/2 -translate-y-1/2 pointer-events-none text-uwu font-regular",
-              hasIcon ? "pl-10" : "pl-4",
-              isFocused || value ? "opacity-0" : "opacity-100"
+              'absolute top-1/2 -translate-y-1/2 pointer-events-none text-uwu font-regular',
+              hasIcon ? 'pl-10' : 'pl-4',
+              isFocused || value ? 'opacity-0' : 'opacity-100'
             )}
           >
             {placeholder}
           </div>
         )}
         <input
-          type={type}
+          type={type === 'password' && showPassword ? 'text' : type}
           name={name}
           value={value}
           onChange={change}
@@ -81,31 +88,48 @@ const InputField: React.FC<InputFieldProps> = ({
           required
           className={twMerge(
             `w-full pl-12 pr-4 ring-2 bg-inactive duration-150 font-regular border-none rounded-md focus:outline-none focus:ring-4 focus:ring-purple`,
-            hasIcon ? "pl-10" : "pl-4",
+            hasIcon ? 'pl-10' : 'pl-4',
             hasMicrointeractions
               ? twMerge(
-                  !error && value ? "ring-green" : "ring-uwu",
-                  isFocused || value ? "font-bold pt-6 pb-2" : "py-4",
-                  error && "ring-red"
+                  !error && value ? 'ring-green' : 'ring-uwu',
+                  isFocused || value ? 'font-bold pt-6 pb-2' : 'py-4',
+                  error && 'ring-red'
                 )
-              : twMerge("py-4", "ring-uwu", error && "ring-red")
+              : twMerge('py-4', 'ring-uwu', error && 'ring-red')
           )}
         />
         {hasIcon && (
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <DynamicIcon
-              iconName={icon || "defaultIcon"}
+              iconName={icon || 'defaultIcon'}
               size="20"
-              color={twMerge(isFocused ? "text-purple" : "text-dark")}
+              color={twMerge(isFocused ? 'text-purple' : 'text-dark')}
             />
           </div>
         )}
-        {hasMicrointeractions && hasIcon && !isSearch && !error && value && (
-          <div className="absolute text-green inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-            {/* lottie?? */}
-            <DynamicIcon iconName={"Check"} size={"24"} />
+        {type === 'password' && hasMicrointeractions && (
+          <div
+            className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+            onClick={handleTogglePassword}
+          >
+            <DynamicIcon
+              iconName={showPassword ? 'Eye_hide' : 'Eye'}
+              size="20"
+              color="text-dark"
+            />
           </div>
         )}
+        {type !== 'password' &&
+        hasMicrointeractions &&
+        hasIcon &&
+        !isSearch &&
+        !error &&
+        value &&
+        isValid ? (
+          <div className="absolute text-green inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+            <DynamicIcon iconName={'Check'} size={'24'} />
+          </div>
+        ) : null}
       </div>
 
       {hasMicrointeractions && error && (
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,12 @@
-import { TopContextBar } from "../components/TopContextBar";
-import GlowBoyz from "../components/GlowBoyz";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import InputField from "../components/InputField";
-import Button from "../components/Button";
-import PasswordStrengthBar from "../components/PasswordStrengthBar";
-import { useSelector } from "react-redux";
-import { AppState } from "../store/store";
+import { TopContextBar } from '../components/TopContextBar';
+import GlowBoyz from '../components/GlowBoyz';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import InputField from '../components/InputField';
+import Button from '../components/Button';
+import PasswordStrengthBar from '../components/PasswordStrengthBar';
+import { useSelector } from 'react-redux';
+import { AppState } from '../store/store';
 
 const Register = () => {
   const hasMicrointeractions = useSelector(
@@ -15,43 +15,43 @@ const Register = () => {
 
   const validationSchema = Yup.object({
     username: Yup.string()
-      .min(4, "Username muss mindestens 4 Zeichen lang sein.")
-      .required("Username ist erforderlich"),
+      .min(4, 'Username muss mindestens 4 Zeichen lang sein.')
+      .required('Username ist erforderlich'),
     email: Yup.string()
-      .email("Geben Sie eine gültige E-Mail-Adresse ein.")
-      .required("E-Mail ist erforderlich"),
+      .email('Geben Sie eine gültige E-Mail-Adresse ein.')
+      .required('E-Mail ist erforderlich'),
     password: Yup.string()
-      .min(6, "Passwort muss mindestens 6 Zeichen lang sein.")
-      .required("Passwort ist erforderlich"),
+      .min(6, 'Passwort muss mindestens 6 Zeichen lang sein.')
+      .required('Passwort ist erforderlich'),
     confirmPassword: Yup.string()
       .oneOf(
-        [Yup.ref("password"), undefined],
-        "Passwörter stimmen nicht überein."
+        [Yup.ref('password'), undefined],
+        'Passwörter stimmen nicht überein.'
       )
-      .required("Passwort bestätigen ist erforderlich"),
+      .required('Passwort bestätigen ist erforderlich')
   });
 
   const formik = useFormik({
     initialValues: {
-      username: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      console.log("Registration successful", values);
-    },
+      console.log('Registration successful', values);
+    }
   });
 
   return (
-    <div className="fixed inset-0 px-5 h-full flex flex-col justify-between gap-4">
+    <div className="fixed inset-0 px-5 h-full flex flex-col justify-between gap-4 overflow-y-auto">
       <TopContextBar
-        headline={"Registrierung"}
-        metaDescription={"Account erstellen"}
-        rightIcon={"Close"}
+        headline={'Registrierung'}
+        metaDescription={'Account erstellen'}
+        rightIcon={'Close'}
         rightIconBg={true}
-        rightIconBgColour={"bg-dark"}
+        rightIconBgColour={'bg-dark'}
         rightIconLink="/login"
         bg="bg-light"
       />
@@ -70,46 +70,49 @@ const Register = () => {
             className="flex flex-col gap-4 w-full mt-4 z-90"
           >
             <InputField
-              type={"text"}
-              name={"username"}
-              placeholder={"Benutzername"}
+              type={'text'}
+              name={'username'}
+              placeholder={'Benutzername'}
               value={formik.values.username}
               change={formik.handleChange}
               blur={formik.handleBlur}
-              icon={"User"}
+              icon={'User'}
+              isValid={formik.touched.username && !formik.errors.username}
               error={
                 formik.touched.username && formik.errors.username
                   ? formik.errors.username
-                  : ""
+                  : ''
               }
             />
             <InputField
-              type={"email"}
-              name={"email"}
-              placeholder={"E-Mail Addresse"}
+              type={'email'}
+              name={'email'}
+              placeholder={'E-Mail Addresse'}
               value={formik.values.email}
               change={formik.handleChange}
               blur={formik.handleBlur}
-              icon={"Envelope"}
+              icon={'Envelope'}
+              isValid={formik.touched.email && !formik.errors.email}
               error={
                 formik.touched.email && formik.errors.email
                   ? formik.errors.email
-                  : ""
+                  : ''
               }
             />
             <div>
               <InputField
-                type={"password"}
-                name={"password"}
-                placeholder={"Passwort"}
+                type={'password'}
+                name={'password'}
+                placeholder={'Passwort'}
                 value={formik.values.password}
                 change={formik.handleChange}
                 blur={formik.handleBlur}
-                icon={"Lock"}
+                icon={'Lock'}
+                isValid={formik.touched.password && !formik.errors.password}
                 error={
                   formik.touched.password && formik.errors.password
                     ? formik.errors.password
-                    : ""
+                    : ''
                 }
               />
               {hasMicrointeractions && (
@@ -117,23 +120,26 @@ const Register = () => {
               )}
             </div>
             <InputField
-              type={"password"}
-              name={"confirmPassword"}
-              placeholder={"Passwort bestätigen"}
+              type={'password'}
+              name={'confirmPassword'}
+              placeholder={'Passwort bestätigen'}
               value={formik.values.confirmPassword}
               change={formik.handleChange}
               blur={formik.handleBlur}
-              icon={"Lock"}
+              icon={'Lock'}
+              isValid={
+                formik.touched.confirmPassword && !formik.errors.confirmPassword
+              }
               error={
                 formik.touched.confirmPassword && formik.errors.confirmPassword
                   ? formik.errors.confirmPassword
-                  : ""
+                  : ''
               }
             />
             <Button
-              label={" Account erstellen"}
-              style={"btn-primary"}
-              link={"/"}
+              label={' Account erstellen'}
+              style={'btn-primary'}
+              link={'/'}
               disabled={!formik.isValid}
             ></Button>
           </form>
