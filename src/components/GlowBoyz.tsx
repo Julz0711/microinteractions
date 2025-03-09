16a91b5f4474185945caf62cc366b3a004e9e065
Markus Essing <essing-markus@web.de>
2025-03-09 16:10:48 +0100

    added house to glowyboyz

--- src/components/GlowBoyz.tsx
+++ src/components/GlowBoyz.tsx
@@ -1,8 +1,9 @@
-import Lottie from 'react-lottie';
-import glowBoyzAnimation from '../assets/lottie/glow_boyz_animation.json';
-import { twMerge } from 'tailwind-merge';
-import { useSelector } from 'react-redux';
-import { AppState } from '../store/store';
+import Lottie from "react-lottie";
+import glowBoyzAnimation from "../assets/lottie/glow_boyz_animation.json";
+import { twMerge } from "tailwind-merge";
+import { useSelector } from "react-redux";
+import { AppState } from "../store/store";
+import DynamicIcon from "./DynamicIcon";
 
 type GlowBoyzProps = {
   isGray?: boolean;
@@ -15,15 +16,19 @@ const GlowBoyz: React.FC<GlowBoyzProps> = ({ isGray }) => {
   return (
     <div
       className={twMerge(
-        isGray && 'grayscale-100',
-        !hasMicrointeractions && 'pointer-events-none'
+        isGray && "grayscale-100",
+        !hasMicrointeractions && "pointer-events-none",
+        "relative"
       )}
     >
+      <span className="absolute left-1/2 top-1/2 transform -translate-x-[45%] -translate-y-[45%] z-10">
+        <DynamicIcon iconName="Home" size="24" />
+      </span>
       <Lottie
         options={{
           loop: hasMicrointeractions ? true : false,
           autoplay: hasMicrointeractions ? true : false,
-          animationData: glowBoyzAnimation
+          animationData: glowBoyzAnimation,
         }}
       />
     </div>
