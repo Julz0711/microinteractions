7120cdbbd883ef059f4af0bcd229ae38832c4099
Markus Essing <essing-markus@web.de>
2025-03-09 18:50:30 +0100

    fix slider

--- src/components/DeviceSettings/LightsComponent.tsx
+++ src/components/DeviceSettings/LightsComponent.tsx
@@ -1,9 +1,9 @@
-import { Slider } from '../Slider/Slider';
-import { useDispatch, useSelector } from 'react-redux';
-import { setIsOn } from '../../store/reducer';
-import { useEffect, useState } from 'react';
-import { AppState } from '../../store/store';
-import { twMerge } from 'tailwind-merge';
+import { Slider } from "../Slider/Slider";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsOn } from "../../store/reducer";
+import { useEffect, useState } from "react";
+import { AppState } from "../../store/store";
+import { twMerge } from "tailwind-merge";
 
 export function LightsComponent() {
   const dispatch = useDispatch();
@@ -11,36 +11,41 @@ export function LightsComponent() {
     (state: AppState) => state.app.hasMicrointeractions
   );
   const [sliderValue, setSliderValue] = useState(75);
-  const [, setIconColor] = useState('text-light');
+  const [slider2Value, setSlider2Value] = useState(50);
+  const [, setIconColor] = useState("text-light");
   const isOn = useSelector((state: AppState) => state.app.isOn);
 
   const handleSliderChange = (value: number) => {
     setSliderValue(value);
     if (value === 0) {
       dispatch(setIsOn(false));
-      setIconColor('text-uwu');
+      setIconColor("text-uwu");
     } else {
       dispatch(setIsOn(true));
-      setIconColor('text-light');
+      setIconColor("text-light");
     }
+  };
+
+  const handleSlider2Change = (value: number) => {
+    setSlider2Value(value);
   };
 
   useEffect(() => {
     if (!isOn) {
       setSliderValue(0);
-      setIconColor('text-uwu');
+      setIconColor("text-uwu");
     } else {
       setSliderValue(75);
-      setIconColor('text-light');
+      setIconColor("text-light");
     }
   }, [isOn]);
 
   const colors = [
-    'bg-[#8AB7FB]',
-    'bg-[#B6D1FA]',
-    'bg-[#F8F8F8]',
-    'bg-[#FDCB9F]',
-    'bg-[#FC983D]'
+    "bg-[#8AB7FB]",
+    "bg-[#B6D1FA]",
+    "bg-[#F8F8F8]",
+    "bg-[#FDCB9F]",
+    "bg-[#FC983D]",
   ];
 
   return (
@@ -51,23 +56,24 @@ export function LightsComponent() {
             <Slider
               hasGradient={false}
               isPx={true}
-              size={'250'}
+              size={"250"}
               clickable={true}
               onChange={handleSliderChange}
               hasIcon={true}
               icon="Lamp"
-              iconColor={'text-light'}
+              iconColor={"text-light"}
               value={sliderValue}
             />
             {!hasMicrointeractions && <p>Helligkeit</p>}
           </div>
-          <div className={twMerge('flex flex-col items-center')}>
+          <div className={twMerge("flex flex-col items-center")}>
             <Slider
               hasGradient={true}
               isPx={true}
-              size={'250'}
+              size={"250"}
               clickable={isOn ? true : false}
-              onChange={() => {}}
+              onChange={handleSlider2Change}
+              value={slider2Value}
             />
             {!hasMicrointeractions && <p>Lichttemperatur</p>}
           </div>
@@ -79,7 +85,7 @@ export function LightsComponent() {
               <div
                 key={index}
                 className={twMerge(
-                  'w-12 h-12 bg-dark rounded-full shadow-2xl border-2 border-[#00000033]',
+                  "w-12 h-12 bg-dark rounded-full shadow-2xl border-2 border-[#00000033]",
                   color
                 )}
               ></div>
